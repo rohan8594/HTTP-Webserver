dadb7e7e8a7d5a68daa42f6c0b45eb63deacc1a4
Jake Carter <jakecarter2012@me.com>
2018-09-15 17:34:14 -0700

    Added response constructing functions

--- http/response/ResponseFactory.java
+++ http/response/ResponseFactory.java
@@ -9,8 +9,6 @@ public class ResponseFactory {
     
     public Response getResponse(Request request, Resource resource)
     {
-        Response response = new Response(resource);
-        
         if(resource.isProtected())
         {
             /*
@@ -32,14 +30,43 @@ public class ResponseFactory {
         File path = new File(resource.absolutePath());
         if(!path.exists())
         {
-            response.setCode(404);
-            response.setReasonPhrase("Not Found");
-            return response;
+            return notFound(resource);
         }
         
         if(resource.isScript())
         {
-            try 
+            return scriptRequest(request, resource);
+        }
+        
+        switch (request.getVerb()) {
+            case "PUT":
+                return PUTrequest(request, resource);
+            case "DELETE":
+                return DELETErequest(resource);
+            case "POST":
+                return POSTrequest(request, resource);
+            case "GET":
+                return GETrequest(request, resource);
+            case "HEAD":
+                return HEADrequest(request, resource);
+            default:
+                return error500(resource);
+        }
+    }
+    
+    private Response notFound(Resource resource)
+    {
+        Response response = new Response(resource);
+        response.setCode(404);
+        response.setReasonPhrase("Not Found");
+        return response;
+    }
+    
+    private Response scriptRequest(Request request, Resource resource)
+    {
+        Response response = new Response(resource);
+        
+        try 
             {
                 //HANDLE IN SEPERATE CLASS
                 ProcessBuilder processBuilder = new ProcessBuilder();
@@ -54,64 +81,80 @@ public class ResponseFactory {
             } 
             catch (IOException e) 
             {
-                response.setCode(500);
-                response.setReasonPhrase("Internal Server Error");
-                return response;
+                return error500(resource);
             }
-        }
+    }
+    
+    private Response PUTrequest(Request request, Resource resource)
+    {
+        Response response = new Response(resource);
+        //generate 201 for new file, 200 for replace old file, 204 for replace old file with empty file
+        //body into stdin for server scripts?
+        response.setCode(201);
+        response.setReasonPhrase("Created");
+        response.addHeader("Content-Location: " + resource.absolutePath());
         
-        if(request.getVerb().equals("PUT"))
+        return response;
+    }
+    
+    private Response DELETErequest(Resource resource)
+    {
+        Response response = new Response(resource);
+        File path = new File(resource.absolutePath());
+        
+        if(path.delete())
         {
-            //generate 201 for new file, 200 for replace old file, 204 for replace old file with empty file
-            //body into stdin for server scripts?
-            response.setCode(201);
-            response.setReasonPhrase("Created");
-            response.addHeader("Content-Location: " + resource.absolutePath());
-            return response;
-        }
-        else if(request.getVerb().equals("DELETE"))
-        {
-            if(path.delete())
-            {
-                response.setCode(204);
-                response.setReasonPhrase("No Content");
-            }
-            else
-            {
-                response.setCode(500);
-                response.setReasonPhrase("Internal Server Error");
-            }
-            return response;
-        }
-        else if(request.getVerb().equals("POST"))
-        {
-            //Send data to server
-            response.setCode(200);
-            response.setReasonPhrase("OK");
-            return response;
-        }
-        else if(request.getVerb().equals("GET"))
-        {
-            //return 304 if unmodified in client's cache, 
-            //otherwise replace with request body and return 200
-            response.setCode(200);
-            response.setReasonPhrase("OK");
-            response.setCode(304);
-            response.setReasonPhrase("Not Modified");
-            return response;
-        }
-        else if(request.getVerb().equals("HEAD"))
-        {
-            //only send headers of get request, not body
-            response.setCode(200);
-            response.setReasonPhrase("OK");
-            return response;
+            response.setCode(204);
+            response.setReasonPhrase("No Content");
         }
         else
         {
             response.setCode(500);
             response.setReasonPhrase("Internal Server Error");
-            return response;
         }
+        
+        return response;
+    }
+    
+    private Response POSTrequest(Request request, Resource resource)
+    {
+        Response response = new Response(resource);
+        //Send data to server
+        response.setCode(200);
+        response.setReasonPhrase("OK");
+        
+        return response;
+    }
+    
+    private Response GETrequest(Request request, Resource resource)
+    {
+        Response response = new Response(resource);
+        //return 304 if unmodified in client's cache, 
+        //otherwise replace with request body and return 200
+        response.setCode(200);
+        response.setReasonPhrase("OK");
+        response.setCode(304);
+        response.setReasonPhrase("Not Modified");
+        
+        return response;
+    }
+    
+    private Response HEADrequest(Request request, Resource resource)
+    {
+        Response response = new Response(resource);
+        //only send headers of get request, not body
+        response.setCode(200);
+        response.setReasonPhrase("OK");
+        
+        return response;
+    }
+    
+    private Response error500(Resource resource)
+    {
+        Response response = new Response(resource);
+        response.setCode(500);
+        response.setReasonPhrase("Internal Server Error");
+        
+        return response;
     }
 }
